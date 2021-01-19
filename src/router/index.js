c96ff44b50d34c9a88366818c6623ccfb127c681
Lenin Sheikh <justtlenin@gmail.com>
2021-01-19 20:58:03 +0600

    added example for Ref

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import CompositionAPI from '../views/CompositionAPI'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/composition',
+    name: 'composition',
+    component: CompositionAPI
   },
   {
     path: '/about',
