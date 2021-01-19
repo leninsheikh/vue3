b908d7931554323ae6318640eb2765919a1732f8
Lenin Sheikh <justtlenin@gmail.com>
2021-01-19 21:00:30 +0600

    vue router added

---
+++ src/components/Ref.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h2>count: {{count}}</h2>
+      <button @click="handleClick">click</button>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+    setup() {
+        const count = ref(0);
+        const handleClick = () => {
+            /**
+             * ref() return a reactive object which contain reactive value.
+             */
+            count.value++;
+            console.log("COUNT", count);
+            console.log("COUNT", count.value);
+        }
+        return {count, handleClick}
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/CompositionAPI.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <Ref />
+  </div>
+</template>
+
+<script>
+import Ref from '@/components/Ref'
+
+export default {
+    components: {Ref},
+    setup() {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
