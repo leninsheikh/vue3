c3adbbe5d56e7ac589cb686289fb74f6635957cf
Lenin Sheikh <justtlenin@gmail.com>
2021-01-19 21:20:41 +0600

    added example for Lifecycle hooks

---
+++ src/components/Lifecycle.vue
@@ -0,0 +1,24 @@
+<template>
+<div></div>
+</template>
+
+<script>
+import { onMounted, onBeforeMount } from 'vue';
+export default {
+    name: "Lifecycle",
+    mounted() {
+        console.log("Lifecycle:Option: Mounted")
+    },
+    setup() {
+
+        onMounted(() => {
+            console.log("Lifecycle:Composition: Mounted")
+        })
+        onBeforeMount(() => console.log('Lifecycle:Composition: Before Mounted'))
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/CompositionAPI.vue
+++ src/views/CompositionAPI.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
+  <div style="display: flex; flex-direction: column">
       <Ref />
+      <Lifecycle />
   </div>
 </template>
 
 <script>
 import Ref from '@/components/Ref'
+import Lifecycle from '@/components/Lifecycle'
 
 export default {
-    components: {Ref},
+    components: {Ref, Lifecycle},
     setup() {
 
     }
