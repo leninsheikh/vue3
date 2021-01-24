d4924d8cb365c93ad805cac78664f7395147eb8d
Lenin Sheikh <justtlenin@gmail.com>
2021-01-24 21:00:23 +0600

    added Simple Form validation

---
+++ src/components/vee-validate/SimpleForm.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="vertical">
+        <div>
+            <input name="email" v-model="email" />
+            <span>{{ emailError }}</span>
+        </div>
+        <div>
+            <input name="password" v-model="password" type="password" />
+            <span>{{ passwordError }}</span>
+        </div>
+        <button @click="handleValidateAll">Validate All</button>
+    </div>
+</template>
+
+<script>
+import {useField, useForm} from 'vee-validate';
+import { object, string } from 'yup';
+export default {
+    name: "SimpleForm",
+    setup() {
+        // Define a validation schema
+        const schema = object({
+            email: string().required().email(),
+            password: string().required().min(8),
+        });
+
+        // Create a form context with the validation schema
+        const { validate,  } = useForm({
+            validationSchema: schema,
+        });
+
+        // No need to define rules for fields
+        const { value: email, errorMessage: emailError } = useField('email');
+        const { value: password, errorMessage: passwordError } = useField('password');
+
+        const handleValidateAll = async () => {
+            const res = await validate();
+            console.log("SimpleForm: validate Result", res)
+        }
+        return {
+            email,
+            emailError,
+            password,
+            passwordError,
+            handleValidateAll
+        };
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/VeeValidate.vue
+++ src/views/VeeValidate.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="vertical">
+      <h2>Individual Field</h2>
       <SimpleInput/>
+
+      <h2>Form</h2>
+      <SimpleForm/>
   </div>
 </template>
 
 <script>
 import SimpleInput from "@/components/vee-validate/SimpleInput";
+import SimpleForm from "@/components/vee-validate/SimpleForm";
 
 
 export default {
-    components: { SimpleInput },
+    components: { SimpleInput, SimpleForm },
 
     setup() {
 
